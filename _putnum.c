6f37b77c2f7adc8a9105aea99670208005b7f691
Mohammed Hayyoun <3aba9irablog@gmail.com>
2023-03-27 23:52:10 +0000

    putnum

---
+++ _putnum.c
@@ -0,0 +1,118 @@
+#include "main.h"
+/**
+ * _putoctal - Prints an unsigned number in octal notation
+ * @types: Array of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int _putoctal(va_list types, char buffer[],
+			  int flags, int width, int precision, int size)
+{
+
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
+
+	UNUSED(width);
+
+	num = size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 8) + '0';
+		num /= 8;
+	}
+
+	if (flags & F_HASH && init_num != 0)
+		buffer[i--] = '0';
+
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
+/**
+ * _puthexa - Prints an unsigned number in hexadecimal notation
+ * @types: Array of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int _puthexa(va_list types, char buffer[],
+			 int flags, int width, int precision, int size)
+{
+	return (_puthex(types, "0123456789abcdef", buffer,
+					flags, 'x', width, precision, size));
+}
+/**
+ * _puthex_up - Prints an unsigned number in upper hexadecimal notation
+ * @types: Array of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int _puthex_up(va_list types, char buffer[],
+			   int flags, int width, int precision, int size)
+{
+	return (_puthex(types, "0123456789ABCDEF", buffer,
+					flags, 'X', width, precision, size));
+}
+/**
+ * _puthex - Prints a hexadecimal number in lower or upper
+ * @types: Array of arguments
+ * @map_to: Array of values to map the number to
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @flag_ch: Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * @size: Size specification
+ * Return: Number of chars printed
+ */
+int _puthex(va_list types, char map_to[], char buffer[],
+			int flags, char flag_ch, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
+
+	UNUSED(width);
+
+	num = size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = map_to[num % 16];
+		num /= 16;
+	}
+
+	if (flags & F_HASH && init_num != 0)
+	{
+		buffer[i--] = flag_ch;
+		buffer[i--] = '0';
+	}
+
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
